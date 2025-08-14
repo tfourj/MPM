012ee0b8de918d4a2c51b127e51b6c1162df572c
tfourj <48729601+tfourj@users.noreply.github.com>
2025-08-14 20:03:00 +0200

    fix/actions: make actions run if windows is locked

--- src/actions/actions.cpp
+++ src/actions/actions.cpp
@@ -5,19 +5,54 @@
 
 #ifdef Q_OS_WIN
 #include <windows.h>
+#include <powrprof.h>
 #endif
 
 static bool executeWin(ActionType type, const QString &exePath)
 {
 #ifdef Q_OS_WIN
+    //Enable privilege on the current process
+    auto enablePrivilege = [](const wchar_t *privName) -> bool {
+        HANDLE token = nullptr;
+        if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token)) {
+            return false;
+        }
+        LUID luid{};
+        const BOOL okLookup = LookupPrivilegeValueW(nullptr, privName, &luid);
+        if (!okLookup) {
+            CloseHandle(token);
+            return false;
+        }
+        TOKEN_PRIVILEGES tp{};
+        tp.PrivilegeCount = 1;
+        tp.Privileges[0].Luid = luid;
+        tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+        const BOOL okAdjust = AdjustTokenPrivileges(token, FALSE, &tp, sizeof(tp), nullptr, nullptr);
+        const DWORD err = GetLastError();
+        CloseHandle(token);
+        return okAdjust && (err == ERROR_SUCCESS);
+    };
+
     if (type == ActionType::Shutdown) {
+        // Try with proper privilege otherwise fall back to shutdown command
+        enablePrivilege(SE_SHUTDOWN_NAME);
+        if (ExitWindowsEx(EWX_POWEROFF | EWX_FORCEIFHUNG, SHTDN_REASON_MAJOR_APPLICATION | SHTDN_REASON_MINOR_OTHER | SHTDN_REASON_FLAG_PLANNED)) {
+            return true;
+        }
         return QProcess::startDetached("shutdown", {"/s", "/t", "0"});
     }
     if (type == ActionType::Restart) {
+        enablePrivilege(SE_SHUTDOWN_NAME);
+        if (ExitWindowsEx(EWX_REBOOT | EWX_FORCEIFHUNG, SHTDN_REASON_MAJOR_APPLICATION | SHTDN_REASON_MINOR_OTHER | SHTDN_REASON_FLAG_PLANNED)) {
+            return true;
+        }
         return QProcess::startDetached("shutdown", {"/r", "/t", "0"});
     }
     if (type == ActionType::Suspend || type == ActionType::Sleep) {
-        // Try SetSuspendState via rundll32
+        enablePrivilege(SE_SHUTDOWN_NAME);
+        if (SetSuspendState(FALSE, TRUE, FALSE)) {
+            return true;
+        }
         return QProcess::startDetached("rundll32.exe", {"powrprof.dll,SetSuspendState", "0", "1", "0"});
     }
     if (type == ActionType::OpenExe) {
@@ -25,7 +60,7 @@ static bool executeWin(ActionType type, const QString &exePath)
         return QProcess::startDetached(exePath);
     }
     if (type == ActionType::Lock) {
-        // Lock workstation
+        if (LockWorkStation()) return true;
         return QProcess::startDetached("rundll32.exe", {"user32.dll,LockWorkStation"});
     }
 #endif
